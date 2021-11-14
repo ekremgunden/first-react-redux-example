458ca3dc50dd802d011fbaaa4f45e5fec37787aa
ekremgunden <eko1128244@gmail.com>
2021-11-14 19:02:50 +0300

    counter example with react-redux

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import counterReducer from './stores/counter'
+
+export default configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
